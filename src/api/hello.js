2897f3bd66931a1244cd8c6a0ac7dcf40ac22714
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-04-02 16:04:33 +0200

    chngs

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,7 +1,7 @@
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.TM_API_KEY,
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_TN_API_KEY,
     'X-RapidAPI-Host': 'transfermarket.p.rapidapi.com'
   }
 };
@@ -9,12 +9,11 @@ const options = {
 export async function getPlayers() {
   let response = await fetch('https://transfermarket.p.rapidapi.com/search?query=chelsea&domain=de', options)
   let data = await response.json()
-
   return data
 }
 
-// export async function getRecipes(fapi) {
-//   let response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${fapi}&number=9`)
-//   let data = await response.json()
-//   return data
-// }
+export async function getRecepies(){
+  const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.NEXT_PUBLIC_RECIPE_API_KEY}&number=9`)
+  const data = await response.json()
+  return data
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,34 @@
-import { useEffect } from 'react';
-// import { getPlayers, getRecipes } from '@/api/hello';
-
-// const key = process.env.TM_API_KEY
-// console.log(key);
-
+import { useEffect, useState } from 'react';
+import { getPlayers, getRecepies } from '@/api/hello';
 
 export default function Home() {
 
+  const [recepies, setRecepies] = useState(null)
 
   const fetchData = async () => {
-    let response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=082510dd42c9428398b5c74503181b7e&number=9`)
-    let data = await response.json()
-    console.log(data);
+    let data = await getRecepies()
+    setRecepies(data.recipes)
   };
 
   useEffect(() => {
-
-    fetchData();
+    fetchData()
   }, []);
-
-
 
   return (
     <>
       <main>
         <h1 className='text-3xl'>Home</h1>
+        <div>
+          <ul>
+            {
+              recepies?.map((recepy, i) => {
+                return (
+                  <li key={i}>{recepy.title}</li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </main>
     </>
   )
