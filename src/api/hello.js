201bb7c203684fff0bc480f4e64a3ae7921c8988
Dmytro Anikin <dimianni@users.noreply.github.com>
2023-04-02 17:05:00 +0200

    Update hello.js

--- src/api/hello.js
+++ src/api/hello.js
@@ -8,12 +8,12 @@ const options = {
 
 export async function getPlayers() {
   let response = await fetch('https://transfermarket.p.rapidapi.com/search?query=chelsea&domain=de', options)
-  let data = await response.json()
+  let data = await response.json();
   return data
 }
 
 export async function getRecepies(){
   const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.NEXT_PUBLIC_RECIPE_API_KEY}&number=9`)
-  const data = await response.json()
+  const data = await response.json();
   return data
 }
